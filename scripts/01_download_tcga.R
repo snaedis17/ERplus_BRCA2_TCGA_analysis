48f96041178acd1d0d9ea0ce7376bce50d2035e3
snaedis17 <44778761+snaedis17@users.noreply.github.com>
2026-03-13 18:36:34 +0000

    01_download_tcga.R

--- scripts/01_download_tcga.R
+++ scripts/01_download_tcga.R
@@ -1,3 +1,15 @@
+# ============================================================
+# Download TCGA BRCA RNA-seq data
+#
+# Steps
+# 1. Query TCGA BRCA RNA-seq STAR counts
+# 2. Download data from GDC
+# 3. Prepare SummarizedExperiment object
+#
+# Output
+# data_raw/tcga_brca_se.rds
+# ============================================================
+
 library(TCGAbiolinks)
 
 query <- GDCquery(
