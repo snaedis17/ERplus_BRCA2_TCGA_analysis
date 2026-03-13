c7f8419ae207d5d6271807b42532742e2868a1f8
snaedis17 <44778761+snaedis17@users.noreply.github.com>
2026-03-13 15:40:15 +0000

    Update 00_utils.R

--- scripts/00_utils.R
+++ scripts/00_utils.R
@@ -1,11 +1,24 @@
 library(dplyr)
 
-norm12 <- function(x) substr(x, 1, 12)
-norm16 <- function(x) substr(x, 1, 16)
+# Normalize TCGA IDs
+norm12 <- function(x) {
+  substr(toupper(trimws(as.character(x))), 1, 12)
+}
 
+norm16 <- function(x) {
+  substr(toupper(trimws(as.character(x))), 1, 16)
+}
+
+# Clean DESeq2 results
 clean_results <- function(res) {
+
   df <- as.data.frame(res)
+
   df$gene <- rownames(df)
-  df <- df %>% filter(!is.na(padj)) %>% arrange(padj)
+
+  df <- df %>%
+    filter(!is.na(padj)) %>%
+    arrange(padj)
+
   return(df)
 }
